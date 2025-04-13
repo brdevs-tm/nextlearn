605c683dd00d7cb9ad06deca9521318a1ed44511
Jahongir Hamidov <bright19.off@gmail.com>
2025-04-13 16:44:43 +0500

    homepage

---
+++ src/app/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="uz">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+// layout.js
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
@@ -10,20 +11,10 @@ export default function RootLayout({ children }) {
   const pathname =
     typeof window !== "undefined" ? window.location.pathname : "";
 
-  const hideNavAndFooter = pathname === "/login"; // login sahifasi uchun yashiramiz
+  const hideNavAndFooter = pathname === "/login";
 
   return (
     <html lang="uz">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className="antialiased">
         {!hideNavAndFooter && <Navbar />}
         {children}
